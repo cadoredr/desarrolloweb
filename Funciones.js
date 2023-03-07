a9fea8cb20885daf68f6a07cf9040c77fb382d0b
cadoredr <122428385+cadoredr@users.noreply.github.com>
2023-03-07 19:01:45 -0300

    Delete Funciones.js

--- Funciones.js
+++
@@ -1,7 +0,0 @@
-function myFunction1() {
-    document.getElementById("Evento").innerHTML = "Hola me llamo Milton Fox, vivo actualmente en la localidad de Ituzango en la provincia de Buenos Aires, estoy estudiando actualmente en la Universidad Nacional de Hurlingham, en la carrera de Redes, estudie varios idiomas como el ingles, frances y aleman atravez de cursos online. hize un curso avanzado de JavaScrip el cual he aprendido mucho y puedo desemvolverme sin problemas en todo lo relacionado con HTML, CSS y JavaScrip. Actualmente sigo con los estudios relacionado al photoshop dado que soy nivel intermedio y quiero ser nivel avanzado.";
-  }
-
-function myFunction(){
-    document.getElementById("Evento1").innerHTML = "Soy una persona capaz de definir lo que quiero y que es mejor para mi. Soy conciente de mi forma de actuar y trato de ser mejor cada dia, sabiendo que puedo lograr grandes metas en mi vida, sere una persona de exito de tal manera que me sienta orgulloso de mi asi como mis padres y contribuire al desarrollo de mi empresa.";
-}
